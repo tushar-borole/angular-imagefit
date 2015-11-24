22978e7c1a6c96450a51f94f0771973b85aa75bc
Tushar Borole <tushariscoolster@gmail.com>
2015-11-24 13:16:40 +0530

    Release v0.0.4

--- angular-imagefit.js
+++ angular-imagefit.js
@@ -19,40 +19,38 @@
         return directive;
 
         function link(scope, element, attrs, controller) {
-            console.log(attrs)
-            $(element).css({"display":'none'});
+            $(element).css({
+                "display": 'none'
+            });
             imagefitOption = {};
             var imagefitOption = scope.$eval(attrs.imagefitOption);
 
             if (attrs.imagefitErrorSrc) {
                 imagefitOption.onError = function (containr, image) {
-console.log(image)
-                    image.attr('src', attrs.imagefitErrorSrc)
-                }
+                    image.attr('src', attrs.imagefitErrorSrc);
+                };
             }
 
             function imageFit() {
                 $timeout(function () {
-
-
-                    //var elementData=$(element).parent().imagefit(imagefitOption);
-
-                    $(element).imageScale(imagefitOption)
+                    $(element).imageScale(imagefitOption);
                 }, 0)
 
             }
 
             attrs.$observe('ngSrc', function () {
-                imageFit()
-            })
-            imageFit()
+                imageFit();
+            });
+            imageFit();
             $(element).on('load', function () {
-                 $(element).css({"display":''});
+                $(element).css({
+                    "display": ''
+                });
                 $(window).resize(function () {
-                    imageFit()
+                    imageFit();
 
-                })
-            })
+                });
+            });
 
 
 
